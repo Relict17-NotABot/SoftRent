e8f2521c71bec7136402686c8f33445c2f837390
¨Rihito¨ <jocaleroji@est.utn.ac.cr>
2026-06-24 14:02:08 -0600

    prueba 1 de reformat

--- js/index.js
+++ js/index.js
@@ -9,11 +9,18 @@ const ecommerceSolutions = document.getElementById("ecommerceSolutions");
 btnCore.addEventListener("click", () => {
     coreSolutions.classList.remove("hidden");
     ecommerceSolutions.classList.add("hidden");
+    btnCore.classList.add("activo");
+    btnEcommerce.classList.remove("activo");
 });
 
 btnEcommerce.addEventListener("click", () => {
     ecommerceSolutions.classList.remove("hidden");
     coreSolutions.classList.add("hidden");
+    btnEcommerce.classList.add("activo");
+    btnCore.classList.remove("activo");
 });
+
+// estado inicial: "Como Trabajamos" activo
+btnCore.classList.add("activo");
 
 /***SECCION DE SERVICIOS END */
