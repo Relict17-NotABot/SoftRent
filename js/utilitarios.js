97ab12bb254069c695d757d0497a29f8f59c5cf4
Relict17-NotABot <ecastross17@gmail.com>
2026-06-29 22:26:18 -0600

    cambios2

--- js/utilitarios.js
+++ js/utilitarios.js
@@ -1,19 +1,3 @@
-btnCore.addEventListener("click", () => {
-    coreSolutions.classList.remove("hidden");
-    ecommerceSolutions.classList.add("hidden");
-    btnCore.classList.add("activo");
-    btnEcommerce.classList.remove("activo");
-});
-
-btnEcommerce.addEventListener("click", () => {
-    ecommerceSolutions.classList.remove("hidden");
-    coreSolutions.classList.add("hidden");
-    btnEcommerce.classList.add("activo");
-    btnCore.classList.remove("activo");
-});
-// x
-btnCore.classList.add("activo");
-
 /**
  * SCROLL DEL HEADER
  */
